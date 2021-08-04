ad865540338f4431b56a5b8a56fc34c14c6d2317
Gustavo Xavier <gustavohsx07@gmail.com>
2021-08-03 23:00:38 -0300

    razao de teste

---
+++ ace2projeto/src/ace2projeto/Hello.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package ace2projeto;
+
+/**
+ * @author guga_
+ *
+ */
+public class Hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("hashuashua");
+
+	}
+
+}
